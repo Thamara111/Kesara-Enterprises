be4c758ad83d06d9c976f3e64d4784213a38fc1a
Thamara111 <thamara.dasun1@gmail.com>
2026-06-30 11:54:07 +0530

    chore: Update database description utility
    - Minor update to the database description script.

--- describe.php
+++ describe.php
@@ -1,4 +1,4 @@
 <?php
 require 'database/connection.php';
-$stmt = $pdo->query('DESCRIBE users');
-print_r($stmt->fetchAll(PDO::FETCH_COLUMN));
+$stmt = $pdo->query('DESCRIBE delivery_assignments');
+print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
